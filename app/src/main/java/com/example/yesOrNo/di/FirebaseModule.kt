478d08e7469c5621b668c8967fa2f4452188bfc6
zmtmqhdl <zmtmqhdl@naver.com>
2025-05-26 23:00:45 +0900

    [update] module

--- app/src/main/java/com/example/yesOrNo/di/FirebaseModule.kt
+++ app/src/main/java/com/example/yesOrNo/di/FirebaseModule.kt
@@ -1,21 +1,30 @@
 package com.example.yesOrNo.di
 
-import com.google.firebase.Firebase
+import com.example.data.repositoryImpl.FirebaseRepository
+import com.example.data.repositoryImpl.FirebaseRepositoryImpl
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.database
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Singleton
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object FirebaseModule {
+abstract class FirebaseModule {
 
-    @Provides
-    @Singleton
-    fun provideFirebaseDatabase(): FirebaseDatabase {
-        return FirebaseDatabase.getInstance()
+    companion object {
+        @Provides
+        @Singleton
+        fun provideFirebaseDatabase(): FirebaseDatabase {
+            return FirebaseDatabase.getInstance()
+        }
     }
+
+    @Binds
+    @Singleton
+    abstract fun bindFirebaseRepository(
+        impl: FirebaseRepositoryImpl
+    ): FirebaseRepository
 }
--- presentation/src/main/java/com/example/presentation/sharedViewModel/WaitingRoomDataViewModel.kt
+++ presentation/src/main/java/com/example/presentation/sharedViewModel/WaitingRoomDataViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.presentation.sharedViewModel
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.data.repositoryImpl.FirebaseRepository
 import com.example.presentation.common.MyWorldViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
